5ae52cd5fda777bb7147f7dbd73545d3cdb67da4
redazzo <gerhardhaak@gmail.com>
2021-09-12 00:48:28 +1200

    Fixing CRLF issues

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-#!/usr/bin/env bash
-GOOS=windows GOARCH=amd64 go build -o bin/rfd.exe ./script/go_modules/rfd
-GOOS=linux GOARCH=amd64 go build -o bin/rfd ./script/go_modules/rfd
-cp ./bin/rfd .
+#!/usr/bin/env bashGOOS=windows GOARCH=amd64 go build -o bin/rfd.exe ./script/go_modules/rfdGOOS=linux GOARCH=amd64 go build -o bin/rfd ./script/go_modules/rfdcp ./bin/rfd .
